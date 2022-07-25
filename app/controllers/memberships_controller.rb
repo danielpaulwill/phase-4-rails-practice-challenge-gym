dd148b0bd59d8282b0b250502a4e63322a95bc32
Daniel Williams <danielpaulwill@gmail.com>
2022-07-25 18:13:42 -0400

    Done.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,5 @@
 class MembershipsController < ApplicationController
-  validates :gym_id, uniqueness: true
+  # validates :gym_id, uniqueness: true
 
   # Not necessary for Lab
   def index
@@ -9,9 +9,9 @@ class MembershipsController < ApplicationController
 
   # Create a new membership part 1 of 2
   def create
-    membership = Membership.new(gym_id: params[:gym_id], client_id: params[:client_id], charge: params[:charge])
+    # membership = Membership.new(gym_id: params[:gym_id], client_id: params[:client_id], charge: params[:charge])
     
-    # membership = Membership.create
+    membership = Membership.create(gym_id: params[:gym_id], client_id: params[:client_id], charge: params[:charge])
     render json: membership
   end
 
